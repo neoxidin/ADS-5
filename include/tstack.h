cd6bc9c11777d6790d9febd53e46fa85033b2ef8
neoxidin <vanya.robotov@gmail.com>
2026-04-18 19:05:04 +0300

    Upd

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,58 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
+#ifndef TSTACK_H_
+#define TSTACK_H_
 
-template<typename T, int size>
+#include <cstddef>
+#include <stdexcept>
+
+template<typename T>
 class TStack {
-  // добавьте код стека
+public:
+    explicit TStack(size_t initialCapacity = 100)
+        : capacity(initialCapacity), topIndex(0) {
+        data = new T[capacity];
+    }
+
+    ~TStack() { delete[] data; }
+
+    void push(const T& value) {
+        if (topIndex >= capacity) {
+            size_t newCapacity = capacity * 2;
+            T* newData = new T[newCapacity];
+            for (size_t i = 0; i < capacity; ++i) {
+                newData[i] = data[i];
+            }
+            delete[] data;
+            data = newData;
+            capacity = newCapacity;
+        }
+        data[topIndex++] = value;
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            throw std::runtime_error("Stack is empty!");
+        }
+        --topIndex;
+    }
+
+    T get() const {
+        if (isEmpty()) {
+            throw std::runtime_error("Stack is empty!");
+        }
+        return data[topIndex - 1];
+    }
+
+    bool isEmpty() const { return topIndex == 0; }
+
+    size_t size() const { return topIndex; }
+
+    void clear() { topIndex = 0; }
+
+private:
+    T* data;
+    size_t capacity;
+    size_t topIndex;
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif  // TSTACK_H_
